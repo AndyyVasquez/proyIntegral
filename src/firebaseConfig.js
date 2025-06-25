8f9cefd2d82620475a0275dfe07b12781de23001
AndreaVasquez <andyy_vasquez_2102@hotmail.com>
2025-06-25 13:37:46 -0600

    Denuevo

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAfEss9H3QEwoLWjkaG_W3kZ0fpVwV_Bko",
@@ -13,4 +13,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export {db};
+export { db };
